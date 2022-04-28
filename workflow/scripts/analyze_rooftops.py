1964b2f93ec514fb4f14f180f40d9654d21f1744
Rodrigo Pueblas <r.pueblas@iwv3.kfa-juelich.de>
2022-04-28 11:57:29 +0200

    Rooftop analysis rule added, and global rule all added

---
+++ workflow/scripts/analyze_rooftops.py
@@ -0,0 +1,29 @@
+import passion
+import argparse, pathlib, yaml, pathlib, shapefile
+import tensorflow as tf
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--config', metavar='C', type=str, help='Config file path')
+args = vars(parser.parse_args())
+configfile = args['config']
+
+with open(configfile, "r") as stream:
+    try:
+        config = yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
+        exit
+
+rooftop_config = config.get('RooftopAnalysis')
+segmentation_config = config.get('ImageSegmentation')
+results_path = pathlib.Path(config.get('results_path'))
+
+input_folder = segmentation_config['output_folder']
+input_path = results_path / input_folder
+
+output_folder = rooftop_config['output_folder']
+output_path = results_path / output_folder
+
+output_name = rooftop_config['file_name']
+
+passion.buildings.rooftop_analysis.generate_rooftops(input_path, output_path, output_name)
