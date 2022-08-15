a94819bfdc84a1fa179383d88ec5b50cc2e0b9f1
r.pueblas <r.pueblas@fz-juelich.de>
2022-08-15 19:49:18 +0200

    Added every step layer to the gui

--- gui/sample_server.py
+++ gui/sample_server.py
@@ -11,16 +11,31 @@ from flask import Flask
 
 import geopandas as gpd
 from shapely.geometry import Polygon
+from shapely.ops import transform
+import shapely
+import pathlib
+import passion
+
+from copy import deepcopy
 
 import folium
 
 app = Flask(__name__)
 
+def open_csv_results(results_path, filename):
+    if (results_path / filename).is_file():
+        # exists
+        results = passion.util.io.load_csv(results_path, filename)
+    else:
+        # does not exist
+        results = None
+    
+    return results
 
 @app.route('/')
 def index():
     start_coords = (50.77595349670269, 6.088548416982711)
-    folium_map = folium.Map(location=start_coords, zoom_start=5)
+    folium_map = folium.Map(location=start_coords, zoom_start=15)
 
     # TILE LAYERS
     folium.TileLayer('cartodbpositron').add_to(folium_map)
@@ -31,42 +46,98 @@ def index():
         overlay = False,
         control = True
        ).add_to(folium_map)
+    
+    outlines = open_csv_results(pathlib.Path('workflow/output/economic/'), 'lcoe.csv')
+    add_outlines_layer(folium_map, outlines, 'economic')
+    
+    outlines = open_csv_results(pathlib.Path('workflow/output/technical/'), 'technical.csv')
+    add_outlines_layer(folium_map, outlines, 'technical')
+    
+    outlines = open_csv_results(pathlib.Path('workflow/output/rooftops/'), 'rooftops.csv')
+    add_outlines_layer(folium_map, outlines, 'rooftops')
+    
 
-    # DEFINE POLYGONS
-    lat_list_1 = [50.854457, 52.518172, 50.072651, 48.853033, 50.854457]
-    lon_list_1 = [4.377184, 13.407759, 14.435935, 2.349553, 4.377184]
-    lat_list_2 = [45.12345, 46.12345, 46.12345, 45.12345]
-    lon_list_2 = [4.12345, 4.12345, 14.12345, 14.12345]
+    outlines = open_csv_results(pathlib.Path('workflow/output/sections/'), 'sections.csv')
+    add_outlines_layer(folium_map, outlines, 'sections')
+    
+    # config.yml bbox frame
+    min_lat = 50.77850739604879
+    min_lon = 6.0768084397936395
+    max_lat = 50.77214558009357
+    max_lon = 6.091035433169415
+    
+    draw_bbox((min_lat, min_lon), (max_lat, max_lon), folium_map)
 
-    polygon_geom_1 = Polygon(zip(lon_list_1, lat_list_1))
-    polygon_geom_2 = Polygon(zip(lon_list_2, lat_list_2))
+    folium.LayerControl().add_to(folium_map)
+    return folium_map._repr_html_()
+
+def add_outlines_layer(map, outlines, name):
+    outlines_latlon_copy = deepcopy(outlines)
+    geo_j = get_geoj_from_latlon_outlines(outlines_latlon_copy)
+    layer_geom = get_layer_from_geoj(name + '-latlon', geo_j)
+    layer_geom.add_to(map)
+    
+    return
+
+def get_geoj_from_latlon_outlines(outlines):
+    for section in outlines:
+        section_geom = shapely.wkt.loads(section['outline_latlon'])
+
+        section_geom = transform(lambda x, y: (y, x), section_geom)
+        section['outline_latlon'] = list(section_geom.exterior.coords)
+
     crs = {'init': 'epsg:4326'}
     polygon_gdf = gpd.GeoDataFrame(
-                                   data=['name1', 'name2'],
-                                   index=[0, 1],
                                    crs=crs,
-                                   geometry=[polygon_geom_1, polygon_geom_2],
-                                   columns=['name']
+                                   geometry=[Polygon(section['outline_latlon']) for section in outlines]
                                    )
-
     geo_j = folium.GeoJson(polygon_gdf)
+
+    return geo_j
+
+def get_layer_from_geoj(name, geo_j):
     
-    layer_geom = folium.FeatureGroup(name='layer1',control=True)
+    layer_geom = folium.FeatureGroup(name=name,control=True)
 
     for feature in geo_j.data['features']:
-        print(feature)
         # GEOJSON layer consisting of a single feature
         temp_layer = folium.GeoJson(feature)
         # create Popup and add it to our lone feature
-        folium.Popup(feature['properties']['name']).add_to(temp_layer)
+        #folium.Popup(feature['properties']['name']).add_to(temp_layer)
 
         # consolidate individual features back into the main layer
         temp_layer.add_to(layer_geom)
-    
-    layer_geom.add_to(folium_map)
-    
-    folium.LayerControl().add_to(folium_map)
-    return folium_map._repr_html_()
+    return layer_geom
+
+def draw_bbox(min_latlon, max_latlon, map):
+    folium.Rectangle(
+        bounds=[
+            [min_latlon[0], min_latlon[1]],
+            [min_latlon[0], max_latlon[1]],
+            [max_latlon[0], max_latlon[1]],
+            [max_latlon[0], min_latlon[1]]
+            ],
+        color='#ff7800',
+        fill=True,
+        fill_color='#ffff00',
+        fill_opacity=0.15,
+        weight=0
+    ).add_to(map)
+    draw_circle(min_latlon[0], min_latlon[1], 'min_lat, min_lon', map, 'black')
+    draw_circle(min_latlon[0], max_latlon[1], 'min_lat, max_lon', map, 'black')
+    draw_circle(max_latlon[0], max_latlon[1], 'max_lat, max_lon', map, 'black')
+    draw_circle(max_latlon[0], min_latlon[1], 'max_lat, min_lon', map, 'black')
+
+def draw_circle(lat, lon, popup, map, color):
+    folium.CircleMarker(
+        location=[lat, lon],
+        popup=popup,
+        radius=4,
+        color=color,
+        fill_color=color,
+        fill_opacity=0.7,
+        weight=0
+        ).add_to(map)
 
 if __name__ == '__main__':
     app.run(debug=True)
