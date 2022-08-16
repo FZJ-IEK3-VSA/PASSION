4d0e9d4a1b788a0bdd57448e64cd9653585fcdf0
r.pueblas <r.pueblas@fz-juelich.de>
2022-08-16 11:16:43 +0200

    Individual popup information as a table and different colours per layer.

--- gui/sample_server.py
+++ gui/sample_server.py
@@ -4,7 +4,6 @@
         $ python server.py
 
     And then head to http://127.0.0.1:5000/ in your browser to see the map displayed
-
 """
 
 from flask import Flask
@@ -19,6 +18,8 @@ import passion
 from copy import deepcopy
 
 import folium
+import branca
+from util import rooftop_popup_html
 
 app = Flask(__name__)
 
@@ -48,37 +49,49 @@ def index():
        ).add_to(folium_map)
     
     outlines = open_csv_results(pathlib.Path('workflow/output/rooftops/'), 'rooftops.csv')
-    add_outlines_layer(folium_map, outlines, 'rooftops')
+    add_outlines_layer(folium_map, outlines, 'rooftops', '#96BDC6')
     
     outlines = open_csv_results(pathlib.Path('workflow/output/sections/'), 'sections.csv')
-    add_outlines_layer(folium_map, outlines, 'sections')
+    add_outlines_layer(folium_map, outlines, 'sections', '#15616D')
     
     outlines = open_csv_results(pathlib.Path('workflow/output/technical/'), 'technical.csv')
-    add_outlines_layer(folium_map, outlines, 'technical')
+    add_outlines_layer(folium_map, outlines, 'technical', '#E3B505')
     
     outlines = open_csv_results(pathlib.Path('workflow/output/economic/'), 'lcoe.csv')
-    add_outlines_layer(folium_map, outlines, 'economic')
+    add_outlines_layer(folium_map, outlines, 'economic', '#81F499')
 
     folium.LayerControl().add_to(folium_map)
     return folium_map._repr_html_()
 
-def add_outlines_layer(map, outlines, name):
+def add_outlines_layer(map, outlines, name, color):
     outlines_latlon_copy = deepcopy(outlines)
     geo_j = get_geoj_from_latlon_outlines(outlines_latlon_copy)
-    layer_geom = get_layer_from_geoj(name + ' layer', geo_j)
+    layer_geom = get_layer_from_geoj(name + ' layer', geo_j, color)
     layer_geom.add_to(map)
     
     return
 
 def get_geoj_from_latlon_outlines(outlines):
+    gdf_data_center = []
+    gdf_data_area = []
     for section in outlines:
         section_geom = shapely.wkt.loads(section['outline_latlon'])
 
         section_geom = transform(lambda x, y: (y, x), section_geom)
         section['outline_latlon'] = list(section_geom.exterior.coords)
+        try:
+            gdf_data_center.append((section['center_lat'], section['center_lon']))
+            gdf_data_area.append(section['area'])
+        except:
+            gdf_data_center.append(None)
+            gdf_data_area.append(None)
 
+    gdf_data = {}
+    gdf_data['gdf_data_center'] = gdf_data_center
+    gdf_data['gdf_data_area'] = gdf_data_area
     crs = {'init': 'epsg:4326'}
     polygon_gdf = gpd.GeoDataFrame(
+                                   data=gdf_data,
                                    crs=crs,
                                    geometry=[Polygon(section['outline_latlon']) for section in outlines]
                                    )
@@ -86,15 +99,26 @@ def get_geoj_from_latlon_outlines(outlines):
 
     return geo_j
 
-def get_layer_from_geoj(name, geo_j):
-    print(geo_j)
-    layer_geom = folium.FeatureGroup(name=name,control=True)
+def get_layer_from_geoj(name, geo_j, color):
+    layer_geom = folium.FeatureGroup(name=name, control=True, show=False)
 
     for feature in geo_j.data['features']:
         # GEOJSON layer consisting of a single feature
-        temp_layer = folium.GeoJson(feature)
+        style = {'fillColor': color, 'lineColor': color, 'color': color}
+        temp_layer = folium.GeoJson(feature, style_function=lambda x:style)
         # create Popup and add it to our lone feature
-        folium.Popup(name).add_to(temp_layer)
+        area = feature['properties']['gdf_data_area']
+        outline_center = feature['properties']['gdf_data_center']
+        if outline_center == None: outline_center = [1,1]
+
+        popup_dict = {
+            'name': 'Rooftop',
+            'area': str(int(area * 100) / 100) + ' m2',
+            'center': (int(outline_center[0] * 10000) / 10000, int(outline_center[1] * 10000) / 10000)
+        }
+        html = rooftop_popup_html(popup_dict)
+        iframe = branca.element.IFrame(html=html,width=300,height=150)
+        folium.Popup(folium.Html(html, script=True), max_width=500).add_to(temp_layer)
 
         # consolidate individual features back into the main layer
         temp_layer.add_to(layer_geom)
