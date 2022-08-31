40d066cc23e26a1d6074e9464c356da8e58c2b78
r.pueblas <r.pueblas@fz-juelich.de>
2022-08-31 11:05:21 +0200

    Added minor config parameters to snakemake

--- workflow/scripts/segment_dataset.py
+++ workflow/scripts/segment_dataset.py
@@ -27,15 +27,31 @@ output_path = results_path / output_folder
 
 is_osm = segmentation_config.get('osm')
 
+polygon_simplification_distance = segmentation_config.get('polygon_simplification_distance')
+polygon_simplification_distance = float(polygon_simplification_distance)
+
+kernel_size = segmentation_config.get('kernel_size')
+kernel_size = int(kernel_size)
+
+osm_request_interval = segmentation_config.get('osm_request_interval')
+osm_request_interval = int(osm_request_interval)
+
 if is_osm:
     passion.segmentation.osm.generate_osm(input_path = input_path,
                     output_path = output_path,
                     save_masks = True,
-                    save_filtered = True)
+                    save_filtered = True,
+                    osm_request_interval = osm_request_interval)
 else:
     model_rel_path = segmentation_config['model_rel_path']
     model_path = results_path / model_rel_path
 
     model = tf.keras.models.load_model(str(model_path))
 
-    passion.segmentation.prediction.segment_dataset(input_path = input_path, model = model, output_path = output_path)
+    passion.segmentation.prediction.segment_dataset(
+        input_path = input_path,
+        model = model,
+        output_path = output_path,
+        polygon_simplification_distance = polygon_simplification_distance,
+        kernel_size = kernel_size
+        )
