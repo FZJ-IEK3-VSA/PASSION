86814e7a12acd5e7488580387e78e0c5274e067a
r.pueblas <r.pueblas@fz-juelich.de>
2022-09-19 19:07:42 +0200

    Updated tensorflow code to store the whole model architecture

--- workflow/scripts/train_segmentation.py
+++ workflow/scripts/train_segmentation.py
@@ -14,17 +14,20 @@ with open(configfile, "r") as stream:
         exit
 
 training_config = config.get('SegmentationTraining')
+results_path = pathlib.Path(config.get('results_path'))
 
-training_data_path = training_config['dataset_folder']
-model_output_path = training_config['output_folder']
+training_data_path = pathlib.Path(training_config['dataset_folder'])
+model_output_path = pathlib.Path(training_config['output_folder']) / training_config['folder_name']
+model_output_path = results_path / model_output_path
 
 batch_size = training_config['batch_size']
 n_epochs = training_config['n_epochs']
 steps_per_epoch = training_config['steps_per_epoch']
 val_steps = training_config['val_steps']
 
-passion.segmentation.training.train_model(training_data_path, model_output_path,
-                                            batch_size=1,
-                                            n_epochs=n_epochs,
-                                            steps_per_epoch=steps_per_epoch,
-                                            val_steps=val_steps)
+passion.segmentation.training.train_model(training_data_path,
+                                          model_output_path,
+                                          batch_size=1,
+                                          n_epochs=n_epochs,
+                                          steps_per_epoch=steps_per_epoch,
+                                          val_steps=val_steps)
